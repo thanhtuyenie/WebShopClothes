e40b9ded8197aafdda198ae4eebd3c9d022ef407
MeiNhuY <ymtn.23it@vku.udn.vn>
2024-11-02 10:13:55 +0700

    Y - footer

--- views/header_footer/footer.php
+++ views/header_footer/footer.php
@@ -1,116 +1,129 @@
-<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" />
-<script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+<head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" />
+  <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMP3/1p2AI9K2rwztjdFn5I5zQJm7aSrlX1pqYz" crossorigin="anonymous">
 </head>
-<div class="untree_co-section bg-light">
-    <div class="container">
-      <div class="row align-items-stretch">
-        <div class="col-12 col-sm-6 col-md-4 mb-3 mb-md-0">
-          <div class="feature h-100" data-aos="fade-up" data-aos-anchor-placement="center-center">
-            <div class="icon mb-4">
-              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-truck" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5v7h-1v-7a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .5.5v1A1.5 1.5 0 0 1 0 10.5v-7zM4.5 11h6v1h-6v-1z"/>
-                <path fill-rule="evenodd" d="M11 5h2.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5h-1v-1h1a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4.5h-1V5zm-8 8a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 1a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/>
-                <path fill-rule="evenodd" d="M12 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 1a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/>
-              </svg>
-            </div>
-            <h3>Vận chuyển</h3>
-            <p>Free ship cho đơn hàng trên 200.000VND trên toàn quốc </p>
-          </div>
+
+<div class="site-footer">
+  <div class="container">
+    <div class="row justify-content-between">
+      <div class="col-lg-3">
+        <div class="widget mb-4">
+          <h1 class="mb-2">Aura Fashion</h1>
+          <p>Luôn dành những sản phẩm tốt nhất cho bạn. Cảm ơn đã tin tưởng chúng tôi!</p>
         </div>
-        <div class="col-12 col-sm-6 col-md-4 mb-3 mb-md-0">
-          <div class="feature h-100" data-aos="fade-up" data-aos-anchor-placement="center-center">
-            <div class="icon mb-4">
-              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-shield-lock" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M5.443 1.991a60.17 60.17 0 0 0-2.725.802.454.454 0 0 0-.315.366C1.87 7.056 3.1 9.9 4.567 11.773c.736.94 1.533 1.636 2.197 2.093.333.228.626.394.857.5.116.053.21.089.282.11A.73.73 0 0 0 8 14.5c.007-.001.038-.005.097-.023.072-.022.166-.058.282-.111.23-.106.525-.272.857-.5a10.197 10.197 0 0 0 2.197-2.093C12.9 9.9 14.13 7.056 13.597 3.159a.454.454 0 0 0-.315-.366c-.626-.2-1.682-.526-2.725-.802C9.491 1.71 8.51 1.5 8 1.5c-.51 0-1.49.21-2.557.491zm-.256-.966C6.23.749 7.337.5 8 .5c.662 0 1.77.249 2.813.525a61.09 61.09 0 0 1 2.772.815c.528.168.926.623 1.003 1.184.573 4.197-.756 7.307-2.367 9.365a11.191 11.191 0 0 1-2.418 2.3 6.942 6.942 0 0 1-1.007.586c-.27.124-.558.225-.796.225s-.526-.101-.796-.225a6.908 6.908 0 0 1-1.007-.586 11.192 11.192 0 0 1-2.417-2.3C2.167 10.331.839 7.221 1.412 3.024A1.454 1.454 0 0 1 2.415 1.84a61.11 61.11 0 0 1 2.772-.815z"/>
-                <path d="M9.5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-                <path d="M7.411 8.034a.5.5 0 0 1 .493-.417h.156a.5.5 0 0 1 .492.414l.347 2a.5.5 0 0 1-.493.585h-.835a.5.5 0 0 1-.493-.582l.333-2z"/>
-              </svg>
-            </div>
-            <h3>An toàn</h3>
-            <p>Vận chuyển đảm bảo sản phẩm còn nguyên vẹn</p>
-          </div>
+        <div class="widget">
+          <h3>Hãy để Aura Fashion thay đổi phong cách sống của bạn</h3>
         </div>
-        <div class="col-12 col-sm-6 col-md-4 mb-3 mb-md-0">
-          <div class="feature h-100" data-aos="fade-up" data-aos-anchor-placement="center-center">
-            <div class="icon mb-4">
-              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-counterclockwise" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M12.83 6.706a5 5 0 0 0-7.103-3.16.5.5 0 1 1-.454-.892A6 6 0 1 1 2.545 5.5a.5.5 0 1 1 .91.417 5 5 0 1 0 9.375.789z"/>
-                <path fill-rule="evenodd" d="M7.854.146a.5.5 0 0 0-.708 0l-2.5 2.5a.5.5 0 0 0 0 .708l2.5 2.5a.5.5 0 1 0 .708-.708L5.707 3 7.854.854a.5.5 0 0 0 0-.708z"/>
-              </svg>
-            </div>
-            <h3>Chất lượng</h3>
-            <p>Sản phẩm luôn chất lượng cao về mặt thẩm mỹ, chất liệu</p>
-          </div>
+      </div>
+
+      <div class="col-lg-3">
+        <div class="widget">
+          <h2 class="text-black">Liên hệ</h2>
+          <ul class="list-unstyled">
+            <li><i class="fas fa-map-marker-alt"></i> <a href="?act=contact">Địa chỉ: 152, Hải Phòng, Hải Châu, Đà Nẵng</a></li>
+            <li><i class="fas fa-clock"></i> <a href="?act=contact">Mở cửa: 7:00AM - 10:00PM</a></li>
+            <li><i class="fas fa-envelope"></i> <a href="?act=contact">Email: AuraFashion@gmail.com</a></li>
+            <li><i class="fas fa-phone-alt"></i> <a href="?act=contact">SDT: 0999003777</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-lg-2">
+        <div class="widget">
+          <h2 class="text-black">Dịch vụ khách hàng</h2>
+          <ul class="list-unstyled">
+            <li><a href="?act=loyalty">Chính sách khách hàng</a></li>
+            <li><a href="?act=delivery">Chính sách thanh toán</a></li>
+            <li><a href="?act=return">Chính sách đổi trả</a></li>
+            <li><a href="?act=privacy">Chính sách bảo mật</a></li>
+            <li><a href="?act=sizeguide">Hướng dẫn chọn size</a></li>
+            <li><a href="?act=partners">Đăng ký đối tác</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-lg-2">
+        <div class="widget">
+          <h2 class="text-black">Tải ứng dụng</h2>
+          <a href="https://play.google.com" target="_blank" class="google-play-link">
+            <span class="google-play-icon">
+              <i class="fab fa-google-play"></i> 
+              <span class="play-text">Google Play</span>
+            </span>
+          </a>
         </div>
       </div>
     </div>
-  </div>
-
-  <div class="site-footer">
-    <div class="container">
-    <div class="row justify-content-between">
-        <div class="col-lg-4">
-            <div class="widget mb-4">
-                <h1 class="mb-2">Aura Fashion</h1>
-            </br>
-                <p>Luôn dành những sản phẩm tốt nhất cho bạn. Cảm ơn đã tin tưởng chúng tôi!</p>
-            </div>
-            <div class="widget">
-                <h3>Hãy để Aura Fashion thay đổi phong cách sống của bạn</h3>
-            
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="widget">
-                <h2>Liên hệ</h2>
-                <ul class="list-unstyled">
-                    <li><a href="?act=contact">Địa chỉ: 152, Hải Phòng, Hải Châu, Đà Nẵng</a></li>
-                    <li><a href="?act=contact">Mở cửa: 7:00AM - 10:00PM</a></li>
-                    <li><a href="?act=contact">Email: AuraFashion@gmail.com</a></li>
-                    <li><a href="?act=contact">SDT: 0999003777</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-lg-2">
-            <div class="widget">
-                <h2>Tài khoản</h2> 
-                <ul class="list-unstyled">
-                    <li><a href="?act=login">Tài khoản</a></li>
-                    <li><a href="?act=cart">Giỏ hàng</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-lg-2">
-            <div class="widget">
-                <h2>Cửa hàng</h2>
-                <ul class="list-unstyled">
-                    <li><a href="?act=home">Trang chủ</a></li>
-                    <li><a href="?act=shop">Cửa hàng</a></li>
-                    <li><a href="?act=sale">Khuyến mãi</a></li>
-                </ul>
-            </div>
-        </div>
-        
-    </div>
-
 
     <div class="row mt-5">
-        <div class="col-12 text-center">
-            <ul class="list-unstyled social">
-                <li><a href="#"><span class="icon-facebook"></span></a></li>
-                <li><a href="#"><span class="icon-instagram"></span></a></li>
-                <li><a href="#"><span class="icon-linkedin"></span></a></li>
-                <li><a href="#"><span class="icon-twitter"></span></a></li>
-            </ul>
-        </div>
-        <div class="col-12 text-center copyright">
-            <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash; Designed with love by <a href="https://aurafashion.com">Aura Fashion</a> 
-
-        </div>
+      <div class="col-12 text-center">
+        <ul class="list-unstyled social">
+          <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+          <li><a href="#"><i class="fab fa-google"></i></a></li>
+          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+          <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+        </ul>
+      </div>
+      <div class="col-12 text-center copyright">
+        <p>&copy;<script>document.write(new Date().getFullYear());</script> Aura Fashion. All Rights Reserved.</p>
+      </div>
     </div>
   </div>
-</div>  
+</div>
+
 <script>
-    AOS.init();
+  AOS.init();
 </script>
 
+<style>
+  .social {
+    display: flex;
+    gap: 10px;
+    justify-content: center;
+    padding: 0;
+  }
+
+  .social li {
+    list-style: none;
+  }
+
+  .social a {
+    color: black;
+    font-size: 1.5em;
+  }
+
+  .google-play-link {
+    color: black;
+    text-decoration: none;
+    font-size: 1.2em;
+    display: flex;
+    align-items: center;
+  }
+
+  .google-play-icon {
+    display: flex;
+    align-items: center;
+    position: relative;
+  }
+
+  .play-text {
+    margin-left: 5px;
+  }
+
+  .google-play-icon i {
+    color: #000;
+    font-size: 1.5em;
+  }
+
+  h2.text-black, h3.text-black {
+    color: black;
+  }
+
+  .list-unstyled li {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+</style>
