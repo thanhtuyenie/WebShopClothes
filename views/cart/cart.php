c935e03c9a2f932a6da3ae89659e694a65bfb649
MeiNhuY <ymtn.23it@vku.udn.vn>
2024-11-02 17:13:57 +0700

    Y - edit Cart, footer, contact

--- views/cart/cart.php
+++ views/cart/cart.php
@@ -5,8 +5,8 @@
     <div class="container">
       <div class="row align-items-end text-center">
         <div class="col-lg-7 mx-auto">
-          <h1>Cart</h1>  
-          <p class="mb-4"><a href="index.html">Home</a> / <strong>Cart</strong></p>        
+          <h1>Giỏ hàng</h1>  
+          <p class="mb-4"><a href="index.html">Trang chủ</a> / <strong>Giỏ hàng</strong></p>        
         </div>
       </div>
     </div>
@@ -22,12 +22,12 @@
             <table class="table table-bordered">
               <thead>
                 <tr>
-                  <th class="product-thumbnail">Image</th>
-                  <th class="product-name">Product</th>
-                  <th class="product-price">Price</th>
-                  <th class="product-quantity">Quantity</th>
-                  <th class="product-total">Total</th>
-                  <th class="product-remove">Remove</th>
+                  <th class="product-thumbnail">Hình ảnh</th>
+                  <th class="product-name">Sản phẩm</th>
+                  <th class="product-price">Giá</th>
+                  <th class="product-quantity">Số lượng</th>
+                  <th class="product-total">Tổng tiền</th>
+                  <th class="product-remove">Xóa</th>
                 </tr>
               </thead>
               <tbody>
@@ -88,22 +88,22 @@
         <div class="col-md-6">
           <div class="row mb-5">
             <div class="col-md-6 mb-3 mb-md-0">
-              <button class="btn btn-black btn-sm btn-block">Update Cart</button>
+              <button class="btn btn-black btn-sm btn-block">Cập nhật giỏ hàng</button>
             </div>
             <div class="col-md-6">
-              <button class="btn btn-outline-black btn-sm btn-block">Continue Shopping</button>
+              <button class="btn btn-outline-black btn-sm btn-block">Quay lại cửa hàng</button>
             </div>
           </div>
           <div class="row">
             <div class="col-md-12">
-              <label class="text-black h4" for="coupon">Coupon</label>
-              <p>Enter your coupon code if you have one.</p>
+              <label class="text-black h4" for="coupon">Mã giảm giá</label>
+              <p>Nhập mã giảm giá của bạn nếu có</p>
             </div>
             <div class="col-md-8 mb-3 mb-md-0">
-              <input type="text" class="form-control py-3" id="coupon" placeholder="Coupon Code">
+              <input type="text" class="form-control py-3" id="coupon" placeholder="Nhập mã giảm giá tại đây">
             </div>
             <div class="col-md-4">
-              <button class="btn btn-black">Apply Coupon</button>
+              <button class="btn btn-black">Áp dụng</button>
             </div>
           </div>
         </div>
@@ -112,7 +112,7 @@
             <div class="col-md-7">
               <div class="row">
                 <div class="col-md-12 text-right border-bottom mb-5">
-                  <h3 class="text-black h4 text-uppercase">Cart Totals</h3>
+                  <h3 class="text-black h4 text-uppercase">Chi tiết đơn hàng</h3>
                 </div>
               </div>
               <div class="row mb-3">
@@ -134,7 +134,10 @@
 
               <div class="row">
                 <div class="col-md-12">
-                  <button class="btn btn-black btn-lg py-3 btn-block" onclick="window.location='checkout.html'">Proceed To Checkout</button>
+                  <!-- <button class="btn btn-black btn-lg py-3 btn-block" onclick="window.location='?act=checkout.php'">Mua hàng</button> -->
+                  <button class="btn btn-black btn-lg py-3 btn-block" href="?act=checkout.php" aria-label="Mua hàng">Mua hàng</button>
+
+                  
                 </div>
               </div>
             </div>
--- views/page/contact.php
+++ views/page/contact.php
@@ -1,14 +1,21 @@
   <!-- headercontact -->
+<header>
+  <style>
+
+  </style>
+
+</header>
+
 <div class="page-heading bg-light">
-    <div class="container">
-      <div class="row align-items-end text-center">
-        <div class="col-lg-7 mx-auto">
-          <h1>Contact</h1>  
-          <p class="mb-4"><a href="index.php">Home</a> / <strong>Contact</strong></p>        
-        </div>
+  <div class="container">
+    <div class="row align-items-end text-center">
+      <div class="col-lg-7 mx-auto">
+        <h1>Liên hệ</h1>  
+        <p class="mb-4"><a href="index.php">Trang chủ</a> / <strong>Liên hệ</strong></p>        
       </div>
     </div>
   </div>
+</div>
 
   
 <!-- deatail -->
@@ -21,13 +28,13 @@
 
             <div class="address mt-4">
               <i class="icon-room"></i>
-              <h4 class="mb-2">Location:</h4>
+              <h4 class="mb-2">Địa chỉ</h4>
               <p>152, Hai Phong, Thach Thang, Hai Chau, Da Nang, Viet Nam</p>
             </div>
 
             <div class="open-hours mt-4">
               <i class="icon-clock-o"></i>
-              <h4 class="mb-2">Open Hours:</h4>
+              <h4 class="mb-2">Giờ mở cửa:</h4>
               <p>
                 Sunday-Friday:<br>
                 7:00 AM - 23:00 PM
@@ -37,13 +44,13 @@
             <div class="email mt-4">
               <i class="icon-envelope"></i>
               <h4 class="mb-2">Email:</h4>
-              <p>kendallstyle@gmail.com</p>
+              <p>Aurafashion@gmail.com</p>
             </div>
 
             <div class="phone mt-4">
               <i class="icon-phone"></i>
-              <h4 class="mb-2">Call:</h4>
-              <p>+1 1234 55488 55</p>
+              <h4 class="mb-2">SĐT</h4>
+              <p>0999003777</p>
             </div>
 
           </div>
@@ -52,20 +59,20 @@
           <form action="#">
             <div class="row">
               <div class="col-6 mb-3">
-                <input type="text" class="form-control" placeholder="Your Name">
+                <input type="text" class="form-control" placeholder="Họ và tên">
               </div>
               <div class="col-6 mb-3">
-                <input type="email" class="form-control" placeholder="Your Email">
+                <input type="email" class="form-control" placeholder="Email">
               </div>
               <div class="col-12 mb-3">
-                <input type="text" class="form-control" placeholder="Subject">
+                <input type="text" class="form-control" placeholder="Tiêu đề">
               </div>
               <div class="col-12 mb-3">
-                <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+                <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Nội dung"></textarea>
               </div>
 
               <div class="col-12">
-                <input type="submit" value="Send Message" class="btn btn-primary">
+                <input type="submit" value="Gửi" class="btn btn-primary">
               </div>
             </div>
           </form>
